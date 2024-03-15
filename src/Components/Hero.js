45a6eb24d31c274b93e92af605aedbfc6b78de46
Jordan-Barnes-Portfolio <jordanbarnesneuro@gmail.com>
2024-03-15 15:32:10 -0400

    Fixed mobile view

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -34,73 +34,74 @@ function Hero() {
   }, []);
 
   return (
-    
+    <div className="main-wrapper">
       <div className="hero-section" style={{margin: "10px;"}}>
         <div style={{position: "absolute", zIndex: "-1", objectFit: "fill"}}>
-        <video
-            height={"absolute"}
-            autoPlay
-            loop
-            muted
-            className="hero-video"
-            src={heroVideo}
-          ></video>
+          <video
+              height={"absolute"}
+              autoPlay
+              loop
+              muted
+              className="hero-video"
+              src={heroVideo}
+            ></video>
+          </div>
+          <div className="text-section" >
+            <p className="text-headline">Need immediate flood or mold services?</p>
+            <h2 className="text-title" style={{width: "100%"}}>
+              Click "Emergency Service" below or call us and we will come right away!
+            </h2>
+            <p className="text-descritpion" style={{width: "100%"}}>
+              Talk to online Technicians and get advice, expert advice, and assistance from
+              one of our techs within minutes. On-demand restoration
+              services at your fingertips.
+            </p>
+            <div style={{display: "-webkit-flex"}}>
+                <div style={{margin: "5px"}}>
+                  <button
+                    className="text-appointment-btn"
+                    type="button"
+                    onClick={handleBookAppointmentClick}
+                  >
+                  <FontAwesomeIcon icon={faCalendarCheck} /> Emergency Service
+                  </button>
+                </div>
+                <div style={{margin: "5px"}}>
+                  <button
+                    className="text-appointment-btn"
+                    type="button"
+                  >
+                  <FontAwesomeIcon icon={faPhone} /> Call: 913 717 8945
+                  </button>
+                </div>
+            </div>
+            <div className="text-stats">
+              <div className="text-stats-container">
+                <p>45k+</p>
+                <p>Received Customers</p>
+              </div>
+
+              <div className="text-stats-container">
+                <p>10+</p>
+                <p>Expert Technicians</p>
+              </div>
+
+              <div className="text-stats-container">
+                <p>10+</p>
+                <p>Years of Experience</p>
+              </div>
+            </div>
         </div>
-        <div className="text-section" >
-          <p className="text-headline">Need immediate flood or mold services?</p>
-          <h2 className="text-title" style={{width: "100%"}}>
-            Click "Emergency Service" below or call us and we will come right away!
-          </h2>
-          <p className="text-descritpion" style={{width: "100%"}}>
-            Talk to online Technicians and get advice, expert advice, and assistance from
-            one of our techs within minutes. On-demand restoration
-            services at your fingertips.
-          </p>
-          <div style={{display: "-webkit-flex"}}>
-              <div style={{margin: "5px"}}>
-                <button
-                  className="text-appointment-btn"
-                  type="button"
-                  onClick={handleBookAppointmentClick}
-                >
-                <FontAwesomeIcon icon={faCalendarCheck} /> Emergency Service
-                </button>
-              </div>
-              <div style={{margin: "5px"}}>
-                <button
-                  className="text-appointment-btn"
-                  type="button"
-                >
-                <FontAwesomeIcon icon={faPhone} /> Call: 913 717 8945
-                </button>
-              </div>
-          </div>
-          <div className="text-stats">
-            <div className="text-stats-container">
-              <p>45k+</p>
-              <p>Received Customers</p>
-            </div>
+        <div className="hero-logo">
+          <img src={logo} alt="logo" width={300} height={300}/>
+        </div>
 
-            <div className="text-stats-container">
-              <p>10+</p>
-              <p>Expert Technicians</p>
-            </div>
-
-            <div className="text-stats-container">
-              <p>10+</p>
-              <p>Years of Experience</p>
-            </div>
-          </div>
-      </div>
-      <div className="hero-logo">
-        <img src={logo} alt="logo" width={300} height={300}/>
-      </div>
-
-      <div
-        onClick={scrollToTop}
-        className={`scroll-up ${goUp ? "show-scroll" : ""}`}
-      >
-        <FontAwesomeIcon icon={faAngleUp} />
+        <div
+          onClick={scrollToTop}
+          className={`scroll-up ${goUp ? "show-scroll" : ""}`}
+        >
+          <FontAwesomeIcon icon={faAngleUp} />
+        </div>
       </div>
     </div>
   );
