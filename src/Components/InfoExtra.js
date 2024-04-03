d1b08a495213e989667bdcc02c1f36b1019ffef1
Jordan-Barnes-Portfolio <jordanbarnesneuro@gmail.com>
2024-04-03 15:48:19 -0400

    Changed description

--- src/Components/InfoExtra.js
+++ src/Components/InfoExtra.js
@@ -32,9 +32,11 @@ function Info() {
                         <span>Why Choose Heartland Restoration</span>
                         </h3>
                         <p className="ba-description">
-                        Discover the reasons to choose Heartland Restoration for your restoration needs.
-                        Experience expert service, convenience, and personalized solutions,
-                        making prompt and efficient service our top priority.
+                        Our team of experienced technicians and flood experts use will 
+                        come at your convenience to assess your home and design personalized 
+                        plans to attack the problem. From comprehensive investigations to 
+                        advanced flood and mold care, we are committed to helping you maintain 
+                        a safe home.
                         </p>
         
                         <p className="ba-checks ba-check-first">
