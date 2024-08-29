3a319d76fbedffc3c2d9697f9988a6373c820826
Jordan-Barnes-Portfolio <jordanbarnesneuro@gmail.com>
2024-08-29 17:18:29 -0400

    Added rsto-resources

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,139 +1,60 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCommentDots,
-  faBars,
-  faXmark,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "../Styles/Navbar.css";
 import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
 import { useMediaQuery } from "react-responsive";
 
 function Navbar() {
-  const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
-  const title = isMobile ? "Heartland" : "Heartland Restoration";
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   const [nav, setNav] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   const openNav = () => {
     setNav(!nav);
-  };
-
-  const handleChatBtnClick = () => {
-    if (!isButtonDisabled) {
-      toast.info("Experiencing high traffic, Please wait a moment.", {
-        position: toast.POSITION.TOP_CENTER,
-        onOpen: () => setIsButtonDisabled(true),
-        onClose: () => setIsButtonDisabled(false),
-      });
-    }
   };
 
   return (
     <div className="navbar-section">
       <h1 className="navbar-title">
         <Link to="/">
-          {title}<span className="navbar-sign"><img ></img></span>
+          {isMobile ? "Heartland" : "Heartland Restoration"}
         </Link>
       </h1>
 
-      {/* Desktop */}
-      <ul className="navbar-items">
-        <li>
-          <Link to="/" className="navbar-links">
-            Home
-          </Link>
-        </li>
-        <li>
-          <a href="#services" className="navbar-links">
-            Services
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="navbar-links">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#reviews" className="navbar-links">
-            Reviews
-          </a>
-        </li>
-        <li>
-          <a href="#Technicians" className="navbar-links">
-            Technicians
-          </a>
-        </li>
-        <li>
-          <a href="/appointment" className="navbar-links">
-            Appointment
-          </a>
-        </li>
-        <li>
-          <a href="https://www.google.com/maps/place//data=!4m3!3m2!1s0x67bbabee79e27d99:0x619c3c1752aca767!12e1?source=g.page.m.ia._&laa=nmx-review-solicitation-ia2" className="navbar-links">
-            Leave a Review
-          </a>
-        </li>
-      </ul>
-
-      {/* <button
-        className="navbar-btn"
-        type="button"
-        disabled={isButtonDisabled}
-        onClick={handleChatBtnClick}
-      >
-        <FontAwesomeIcon icon={faCommentDots} /> Live Chat
-      </button> */}
-
-      {/* Mobile */}
-      <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
-        <div onClick={openNav} className="mobile-navbar-close">
-          <FontAwesomeIcon icon={faXmark} className="hamb-icon" />
-        </div>
-
-        <ul className="mobile-navbar-links">
-          <li>
-            <Link onClick={openNav} to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <a onClick={openNav} href="#services">
-              Services
-            </a>
-          </li>
-          <li>
-            <a onClick={openNav} href="#about">
-              About
-            </a>
-          </li>
-          <li>
-            <a onClick={openNav} href="#reviews">
-              Reviews
-            </a>
-          </li>
-          <li>
-            <a onClick={openNav} href="#Technicians">
-              Technicians
-            </a>
-          </li>
-          <li>
-            <a onClick={openNav} href="#contact">
-              Contact
-            </a>
-          </li>
+      {!isMobile && (
+        <ul className="navbar-items">
+          <li><Link to="/" className="navbar-links">Home</Link></li>
+          <li><a href="#services" className="navbar-links">Services</a></li>
+          <li><a href="#about" className="navbar-links">About</a></li>
+          <li><a href="#reviews" className="navbar-links">Reviews</a></li>
+          <li><a href="#Technicians" className="navbar-links">Technicians</a></li>
+          <li><a href="/appointment" className="navbar-links">Appointment</a></li>
+          <li><a href="https://sites.google.com/heartlandflood.com/resto-resources/resources" className="navbar-links">Resto-Resources</a></li>
+          <li><a href="https://www.google.com/maps/place//data=!4m3!3m2!1s0x67bbabee79e27d99:0x619c3c1752aca767!12e1?source=g.page.m.ia._&laa=nmx-review-solicitation-ia2" className="navbar-links">Leave a Review</a></li>
         </ul>
-      </div>
+      )}
 
-      {/* Hamburger Icon */}
-      <div className="mobile-nav">
-        <FontAwesomeIcon
-          icon={faBars}
-          onClick={openNav}
-          className="hamb-icon"
-        />
-      </div>
+      {isMobile && (
+        <>
+          <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+            <div onClick={openNav} className="mobile-navbar-close">
+              <FontAwesomeIcon icon={faXmark} className="hamb-icon" />
+            </div>
+            <ul className="mobile-navbar-links">
+              <li><Link onClick={openNav} to="/">Home</Link></li>
+              <li><a onClick={openNav} href="#services">Services</a></li>
+              <li><a onClick={openNav} href="#about">About</a></li>
+              <li><a onClick={openNav} href="#reviews">Reviews</a></li>
+              <li><a onClick={openNav} href="#Technicians">Technicians</a></li>
+              <li><a onClick={openNav} href="/appointment">Appointment</a></li>
+              <li><a onClick={openNav} href="https://www.google.com/maps/place//data=!4m3!3m2!1s0x67bbabee79e27d99:0x619c3c1752aca767!12e1?source=g.page.m.ia._&laa=nmx-review-solicitation-ia2">Leave a Review</a></li>
+            </ul>
+          </div>
+          <div className="mobile-nav">
+            <FontAwesomeIcon icon={faBars} onClick={openNav} className="hamb-icon" />
+          </div>
+        </>
+      )}
     </div>
   );
 }
